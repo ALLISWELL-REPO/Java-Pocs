fde5036e013b9a6284b6fdcda48291459f9aa062
ALLISWELL-REPO <alliswellrepo@gmail.com>
2019-10-02 00:11:08 +0530

    sixth commit

---
+++ Java Testing/Developer.java
@@ -0,0 +1,14 @@
+class Developer
+{
+	public Developer()
+	{
+		System.out.println("Developer Constructor");
+	}
+
+	public void m1(){
+		System.out.println("Developer class m1() method");
+	}
+	public void m2(){
+		System.out.println("Developer class m2() method");
+	}
+}
