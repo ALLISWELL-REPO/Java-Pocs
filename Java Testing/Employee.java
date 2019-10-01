3fea287e7d29d23b46e51ee0a730fdc6d2a01c83
ALLISWELL-REPO <alliswellrepo@gmail.com>
2019-10-01 23:58:48 +0530

    fourth commit

---
+++ Java Testing/Employee.java
@@ -0,0 +1,15 @@
+class Employee extends Person
+{
+	public Employee(){
+		System.out.println("Employee class Constructor");
+	}
+
+	public void m1(){
+		System.out.println("Employee class m1() method");
+	}
+
+	public void m2(){
+		System.out.println("Employee classs m2() method");
+	}
+
+}
