00f55dc8c16b3836f4dbee6a8a8c278ee188c66b
ALLISWELL-REPO <alliswellrepo@gmail.com>
2019-10-01 23:12:37 +0530

    First commit

---
+++ Java Testing/Person.java
@@ -0,0 +1,14 @@
+class Person 
+{
+	public Person(){
+		System.out.println("Person class Constructor");
+	}
+
+	public void m1(){
+		System.out.println("Person class m1() method");	
+	}
+
+	public void m2(){
+		System.out.println("Person class m2() method");	
+	}
+}
---
+++ Java Testing/Student.java
@@ -0,0 +1,25 @@
+class Student extends Person
+{
+	public Student(){
+		System.out.println("Student class constructor");	
+	}
+
+	public void m1(){
+	System.out.println("This is m1() method of Student class");
+	}
+
+	public void m3(){
+		System.out.println("This is m3() method of Student class");	
+	}
+
+	public static void main(String[] args){
+		Person p=new Person();
+		p.m1();
+		p.m2();
+
+		Student s=new Student();
+		s.m1();
+		s.m2();
+		s.m3();
+	}
+}
